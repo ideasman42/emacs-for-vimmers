da1fe3aa6632bf348f2be78a802690b00eba43f3
Campbell Barton <ideasman42@gmail.com>
2022-04-27 08:46:49 +1000

    Forward arguments from the launcher script

--- emacs-run-local.sh
+++ emacs-run-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BASEDIR=$(realpath $(dirname "$0"))
-env HOME="$BASEDIR" emacs "$BASEDIR/.emacs.d/init.el" --debug-init
+env HOME="$BASEDIR" emacs "$BASEDIR/.emacs.d/init.el" --debug-init "$@"
